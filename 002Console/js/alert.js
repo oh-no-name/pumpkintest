bcdde8805add84aea5c850fb5dc03ec09f13339e
oh-no-name <tingx6606@gmail.com>
2020-04-18 11:55:05 +0930

    1

--- 002Console/js/alert.js
+++ 002Console/js/alert.js
@@ -7,9 +7,9 @@ function taolu(){
 alert("你怎么打开了这个网页");
 alert("我知道你有点儿好奇");
 
-taolu();
+// taolu();
 alert("你相信奇迹吗？");
-alert("让我来向你提个问题");
+// alert("让我来向你提个问题");
 taolu();
 alert("果然没有奇迹");
 alert("我们就这样放弃吗？");
